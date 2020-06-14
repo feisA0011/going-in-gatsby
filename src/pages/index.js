b6f79112714774e399cd2e3bc5a1b3f5abaeea2f
Feisal Mohamud Ahmed <6140236@RTS-365.dk>
2020-06-14 17:48:46 +0200

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,11 @@ import React from "react"
 import Layout from "../components/layout"
 // import { ExampleButton } from "../components/button"
 
-<<<<<<< HEAD
 export default function Home() {
   return <div>Hello world!</div>
 }
-=======
 export default () => (
   <Layout>
     <h1>Hello from gatsby</h1>
   </Layout>
 )
->>>>>>> dev
